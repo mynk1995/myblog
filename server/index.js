ab6fb51d44d64602777336d53b8d0300425bb41d
Mayank Agarwal <mayankagarwal@Mayanks-MacBook-Air.local>
2018-02-25 23:16:59 +0530

    database running and collection will abke to ind

--- server/index.js
+++ server/index.js
@@ -5,12 +5,33 @@ import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev.js';
 import route from './routes/index.js';
+var MongoClient = require('mongodb').MongoClient;
 
 const router = express.Router();
 let app = express();
 const compiler = webpack(webpackConfig);
 
+MongoClient.connect("mongodb://localhost:27017/config", function (err, database) {
+   
+     if(err) throw err;
+       const mydb = database.db('config')
 
+     var collection = mydb.collection('posts');
+   
+        
+
+        collection.find().toArray(function(err, results) {
+        console.dir(results);
+        // Let's close the db
+        
+    });
+    
+
+    database.close();
+
+     //Write databse Insert/Update/Query code here..
+                
+});
 
 for (var x in route ){
 	app.use('/api',require(path.join(__dirname+'/routes/',route[x])));
